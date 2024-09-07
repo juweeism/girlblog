26bf8f5e4517bb23e6ffead5e04701fe67c10ce6
juweeism <mijane.404@gmail.com>
2024-09-07 23:48:53 +0800

    first commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "juwee's girlblog"
+export const SITE_DESCRIPTION = 'we are so back baby';
