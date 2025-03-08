4850f5072c6aa31fea6bc3441a8e25144922255d
juweeism <mijane.404@gmail.com>
2025-03-08 22:43:09 +0800

    add rss content

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,25 @@
 import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
 import { getCollection } from 'astro:content';
+
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
+const parser = new MarkdownIt();
+
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+    const posts = await getCollection('blog');
+    return rss({
+        title: SITE_TITLE,
+        description: SITE_DESCRIPTION,
+        site: context.site,
+        items: posts.map((post) => ({
+            link: `/blog/${post.slug}/`,
+            content: sanitizeHtml(parser.render(post.body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+            }),
+            ...post.data,
+        })),
+    });
 }
